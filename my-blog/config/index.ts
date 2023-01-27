a0af335fa6ae9e16126110ac7a370d1847902795
rain <3100406687@qq.com>
2023-01-27 18:02:48 +0800

    设置服务的session

--- my-blog/config/index.ts
+++ my-blog/config/index.ts
@@ -1,8 +1,9 @@
 export const ironOption = {
-  cookieName: process.env.SESSION_COOKIE_NAME,
-  password:  process.env.SESSION_COOKIE_PASSWORD,
+  cookieName: process.env.SESSION_COOKIE_NAME as string,
+  password: process.env.SESSION_COOKIE_PASSWORD as string,
   // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
+    maxAge: 24 * 60 * 60,
     secure: process.env.NODE_ENV === 'production',
   },
 }
--- my-blog/pages/api/user/sendVerifyCode.ts
+++ my-blog/pages/api/user/sendVerifyCode.ts
@@ -1,28 +1,32 @@
-import { format } from 'date-fns';
+import { format } from 'date-fns'
 import md5 from 'md5'
 import http from '@/utils/http'
 import { encode } from 'js-base64'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { withIronSessionApiRoute } from "iron-session/next";
-
+import { withIronSessionApiRoute } from 'iron-session/next'
+import { ironOption } from '@/config'
+import { ISession } from '@/utils'
 
 /**
- * api路由 http://localhost:3000/api/user/sendVerifyCode
+ * ironSeesion 在内存中保存变量
+ * 通过这个方式包裹起来 req就存在了session const session =res.sesion
+ */
+export default withIronSessionApiRoute(sendVerifyCode, ironOption)
+
+/**
+ * api路由 发送验证码 http://localhost:3000/api/user/sendVerifyCode
  * @param req
  * @param res
  */
-export default async function sendVerifyCode(
-  req: NextApiRequest,
-  response: NextApiResponse
-) {
+async function sendVerifyCode(req: NextApiRequest, response: NextApiResponse) {
+  const session: ISession = req.session
   const { to, templateId } = req.body
   const AccountId = '2c94811c85c276590185f23ba4d902df'
   const appId = '2c94811c85c276590185f23ba5b702e6'
   const AuthToken = 'c172df8ed6474fcb80629d5d5e0d4911'
-  const NowDate =format(new Date(), 'yyyyMMddHHmmss')
+  const NowDate = format(new Date(), 'yyyyMMddHHmmss')
   // 生成SignParameter
   const SigParameter = md5(`${AccountId}${AuthToken}${NowDate}`).toUpperCase()
-  console.log(SigParameter)
   // 生成Authorization
   const Authorization = encode(`${AccountId}:${NowDate}`)
   // 拼接url
@@ -32,7 +36,7 @@ export default async function sendVerifyCode(
   // 过期时间 5分钟
   const expireMinute = '5'
   // 发送请求，获取短信验证码
-  const res = await http.post(
+  const res: any = await http.post(
     url,
     {
       to,
@@ -42,10 +46,16 @@ export default async function sendVerifyCode(
     },
     { headers: { Authorization } }
   )
-  console.log(res)
+  const { statusCode, statusMsg } = res
+  if (statusCode == '000000') {
+    session.verifyCode = verifyCode
+    // 保存session
+    await session.save()
+  }
 
   response.status(200).json({
-    code: 0,
-    data: 1243,
+    code: statusCode,
+    msg: statusMsg,
+    data: {},
   })
 }
